8cc91182ff7d5ce6da61194e8efa783c5e2231b8
michaelw <michaelw@WBHMLPT1157.tmscorp.com>
2019-12-03 12:36:25 -0600

    Refactoring

--- Advent_2019/src/mwarren/advent2019/Advent1.java
+++ Advent_2019/src/mwarren/advent2019/Advent1.java
@@ -1,29 +1,49 @@
 package mwarren.advent2019;
 
-import java.util.*;
-import java.io.*;
+import java.io.File;
+import java.util.Scanner;
 
-public class Advent1 {
+public class Advent1
+{
+    public static void main(String[] args) throws Exception
+    {
+        long total = 0;
+        File file = new File("Advent1_input.txt");
+        Scanner scanner = new Scanner(file);
 
-  public static void main(String[] args) throws Exception {
-	int total = 0;
-	File file = new File("Advent1_input.txt");
-	Scanner scanner = new Scanner(file);
+//        total = part1(scanner);
+        total = part2(scanner);
 
-	while (scanner.hasNextLong()) {
-	  long mass = scanner.nextLong();
-	  while ((mass = findMass(mass)) > 0) {
-		total += mass;
-	  }
-	}
+        System.out.println("total: " + total);
+    }
+    
+    private static long part1(Scanner scanner) {
+        long total = 0;
+        
+        while (scanner.hasNextLong()) {
+            total += findMass(scanner.nextLong());
+        }
 
-	System.out.println("total: " + total);
-	// 3353880 - part 1
-	// 5027950 - part 2
-  }
+        return total;
+    }
 
-  public static long findMass(long input) {
-	long ret = (input / 3) - 2;
-	return (ret > 0) ? ret : 0;
-  }
+    private static long part2(Scanner scanner) {
+        long total = 0;
+        
+        while (scanner.hasNextLong()) {
+            long mass = scanner.nextLong();
+            while ((mass = findMass(mass)) > 0)
+            {
+                total += mass;
+            }
+        }
+
+        return total;
+    }
+
+    public static long findMass(long input)
+    {
+        long ret = (input / 3) - 2;
+        return (ret > 0) ? ret : 0;
+    }
 }
